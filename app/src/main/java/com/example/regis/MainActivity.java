54e08b040608538231e4d73c7f4e06e70954d2d6
Alekseev Korney <yfafcayfaa@gmail.com>
2022-10-04 17:01:21 +0900

    asd

--- app/src/main/java/com/example/regis/MainActivity.java
+++ app/src/main/java/com/example/regis/MainActivity.java
@@ -24,11 +24,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        //init();
     }
 
-    @Override
-    protected void onStart() {
+        @Override
+   protected void onStart() {
         super.onStart();
         FirebaseUser cUser = mAuth.getCurrentUser();
         if(cUser != null)
